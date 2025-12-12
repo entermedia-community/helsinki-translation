525d6a9358af2e7d06036488e41923e583837f53
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2025-12-13 00:25:08 +0600

    Set CUDA_VISIBLE_DEVICES to 1 in run.sh for uvicorn execution

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 export HF_HOME=./models
 killall uvicorn
-CUDA_VISIBLE_DEVICES=0 uvicorn main:app --host 0.0.0.0 --port 5000 --workers 1 &
+CUDA_VISIBLE_DEVICES=1 uvicorn main:app --host 0.0.0.0 --port 5000 --workers 1 &
