306da4effc206ef2f37740efabad26dc5054f6d8
Cristobal Mejia <cristobal.mejia@gmail.com>
2026-09-01 16:47:06 -0600

    interfering?

--- server.sh
+++ server.sh
@@ -6,17 +6,6 @@
 # Kill any process running on port 8600
 lsof -ti :8600 | xargs -r kill -9
 
-# Load environment variables if .env exists
-if [ -f "/root/.env" ]; then
-    set -a
-    source "/root/.env"
-    set +a
-elif [ -f "$(dirname "$0")/.env" ]; then
-    set -a
-    source "$(dirname "$0")/.env"
-    set +a
-fi
-
 #export CUDA_VISIBLE_DEVICES="${CUDA_VISIBLE_DEVICES:-1}"
 export CUDA_VISIBLE_DEVICES=1
 
